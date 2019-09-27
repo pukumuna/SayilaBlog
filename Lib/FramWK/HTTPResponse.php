64355cf59a9eb071024cddb5e3bfd486192287ff
sayila <gesak@orange.fr>
2019-09-27 14:30:21 +0200

    Mise à niveau fichier internaute du framework FramWK

---
+++ Lib/FramWK/HTTPResponse.php
@@ -0,0 +1,77 @@
+<?php
+namespace OCFram;
+
+class HTTPResponse extends ApplicationComponent
+{
+  protected $page;
+
+  public function __construct($app)
+  {
+    parent::__construct($app);
+  }
+
+  public function addHeader($header)
+  {
+    header($header);
+  }
+
+  public function redirect($location)
+  {
+    header('Location: '.$location);
+    echo '<br>PASSAGE DE . REDIRECT';
+    exit;
+  }
+
+    
+  public function send($controller,$internaute)
+  //public function send()
+  {
+    // Actuellement, cette ligne a peu de sens dans votre esprit.
+    // Promis, vous saurez vraiment ce qu'elle fait d'ici la fin du chapitre
+    // (bien que je suis sûr que les noms choisis sont assez explicites !).
+    
+    $internaute->setAttribute('module', $controller->module());
+  
+    exit($controller->page()->getGeneratedPage());
+    //exit($this->page->getGeneratedPage());
+  }
+
+  public function sendRedirect()
+  {
+    exit($this->page->getGeneratedPage());;
+  }
+
+  public function setPage(Page $page)
+  {
+    $this->page = $page;
+  }
+  // Changement par rapport à la fonction setcookie() : le dernier argument est par défaut à true
+  public function setCookie($name, $value = '', $expire = 0, $path = null, $domain = null, $secure = false, $httpOnly = true)
+  {
+    setcookie($name, $value, $expire, $path, $domain, $secure, $httpOnly);
+  }
+  public function redirect404()
+  {
+    $name1 = $this->app()->name();
+    $appDir = $this->app()->appDirectory();
+
+    $this->page = new Page($this->app());
+    $this->page->setContentFile($appDir.'/APP/'.$name1.'/Errors/404.html');
+    
+    $this->addHeader('HTTP/1.0 404 Not Found');
+    
+    $this->sendRedirect();
+  }
+  public function redirect405()
+  {
+    $name1 = $this->app()->name();
+    $appDir = $this->app()->appDirectory();
+
+    $this->page = new Page($this->app());
+    $this->page->setContentFile($appDir.'/APP/'.$name1.'/Errors/405.html');
+    
+    $this->addHeader('HTTP/1.0 405 Not Found');
+    
+    $this->sendRedirect();
+  }
+}
---
+++ Lib/FramWK/Internaute.php
@@ -0,0 +1,94 @@
+<?php
+namespace OCFram;
+
+session_start();
+
+//$_SESSION['auth'] = false;
+//$_SESSION['admin'] = false;
+
+class Internaute extends ApplicationComponent
+{
+  public function __construct($app)
+  {
+    parent::__construct($app);
+  }
+    
+  public function getAttribute($attr)
+  {
+    return isset($_SESSION[$attr]) ? $_SESSION[$attr] : null;
+  }
+
+  public function getFlash()
+  {
+    $flash = $_SESSION['flash'];
+    unset($_SESSION['flash']);
+
+    return $flash;
+  }
+
+  public function hasFlash()
+  {
+    return isset($_SESSION['flash']);
+  }
+
+  public function isFrontend()
+  {
+    return isset($_SESSION['front']) && $_SESSION['front'] === true;
+  }
+
+  public function isHead()
+  {
+    return isset($_SESSION['head']) && $_SESSION['head'] === true;
+  }
+  
+  public function isAuthenticated()
+  {
+    return isset($_SESSION['auth']) && $_SESSION['auth'] === true;
+  }
+
+  public function isAdministrateur()
+  {
+    return isset($_SESSION['admin']) && $_SESSION['admin'] === true;
+  }
+
+  public function setAttribute($attr, $value)
+  {
+    $_SESSION[$attr] = $value;
+  }
+
+  public function setAuthenticated($authenticated)
+  {
+    if (!is_bool($authenticated))
+    {
+      throw new \InvalidArgumentException('La valeur spécifiée à la méthode Internaute::setAuthenticated() doit être un boolean');
+    }
+
+    $_SESSION['auth'] = $authenticated;
+  }
+
+  public function setFrontend($valBool)
+  {
+    if (!is_bool($valBool))
+    {
+      throw new \InvalidArgumentException('La valeur spécifiée à la méthode Internaute::setFronted() doit être un boolean');
+    }
+
+    $_SESSION['front'] = $valBool;
+  }
+
+  public function setAdministrateur($boolval)
+  {
+    if (!is_bool($boolval))
+    {
+      throw new \InvalidArgumentException('La valeur spécifiée à la méthode User::setAdministrateur() doit être un boolean');
+    }
+
+    $_SESSION['admin'] = $boolval;
+  }
+
+  public function setFlash($value)
+  {
+    $_SESSION['flash'] = $value;
+  }
+    
+}
