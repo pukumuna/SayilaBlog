5427f09d80b84e4f3ae53677d1c99d32e291acd4
sayila <gesak@orange.fr>
2019-09-27 09:47:24 +0200

    Création des repertoires pour la partie gestion : admin

---
+++ App/Backend/BackendApplication.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Backend;
+
+use \OCFram\Application;
+
+class BackendApplication extends Application
+{
+  public function __construct($appliDir)
+  {
+    parent::__construct();
+
+    $this->name = 'Backend';
+    $this->appDirectory = $appliDir;
+  }
+
+  public function run()
+  { //echo '<br><br>Run de BackendApplication'; 
+  
+    //if ($this->internaute->isAuthenticated())
+    if ($this->internaute()->isAdministrateur()) {
+        //echo '<br><br><br><br>Gugu : getController';
+      $controller = $this->getController();
+    }
+    else
+    { //echo '<br><br><br><br>Toto : ConnexionController';
+      $controller = new Modules\Connexion\ConnexionController($this, 'Connexion', 'index');
+    }
+    //echo '<br><br><br>Execute de BackendApplication ';
+    $controller->execute();
+
+    //echo '<br><br><br>HttpResponse setpage';
+    
+    //echo '<br><br><br>HttpResponse send';
+    
+    //$this->httpResponse->setPage($controller->page());
+    //$this->httpResponse->send();
+    $this->httpResponse->send($controller,$this->internaute()); 
+
+
+  }
+}
